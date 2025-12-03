8b7216ce388cfd5b8c1d219705c1b0fce5ea73b8
Fouad Mardi <71750191+Mfouad-ardi@users.noreply.github.com>
2025-12-03 14:39:32 +0100

    add speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import ComingSoonNewSection from "./sections/ComingSoonNew";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 function App() {
   return (
     <>
       <div>
         <ComingSoonNewSection />
       </div>
+      <SpeedInsights />
     </>
   );
 }
